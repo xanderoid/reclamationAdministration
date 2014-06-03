36684b092a72262eba741bec6eaf6c2faacb09cc
Xander Taylor <xanderoid@gmail.com>
2014-06-02 21:45:47 -0700

    added 'add space' sidebar to function.php

--- wp-content/themes/oldpaper/functions.php
+++ wp-content/themes/oldpaper/functions.php
@@ -182,6 +182,17 @@ if ( function_exists( 'register_sidebar' ) ){
     ));
 }
 
+if ( function_exists( 'register_sidebar' ) ){
+    register_sidebar(array(
+		'name'=>'Addvert space',
+        'description' => 'Customizable add space',
+        'before_widget' => '<div class="widget col-sm-6 %2$s" id="%1$s">',
+        'after_widget' => '</div>',
+        'before_title' => '<h4><span>',
+        'after_title' => '</span></h4>',
+    ));
+}
+
 
 
 	// custom theme
