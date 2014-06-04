1963c95b53bb9646ccbeb8993d9ea156dc58e0b6
Xander Taylor <xanderoid@gmail.com>
2014-06-04 16:47:02 -0700

    added 728x90px image placeholder to add space

--- wp-content/themes/oldpaper/functions.php
+++ wp-content/themes/oldpaper/functions.php
@@ -188,8 +188,8 @@ if ( function_exists( 'register_sidebar' ) ){
         'description' => 'Customizable add space',
         'before_widget' => '<div class="widget col-sm-12 %2$s" id="%1$s">',
         'after_widget' => '</div>',
-        'before_title' => '<h4><span>',
-        'after_title' => '</span></h4>',
+        //'before_title' => '<h4><span>',
+        //'after_title' => '</span></h4>',
     ));
 }
 
