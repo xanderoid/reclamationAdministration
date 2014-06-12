7c6cef675ff54e2281b0130b34139f71db8adc16
Xander Taylor <xanderoid@gmail.com>
2014-06-12 12:01:36 -0700

    aligned text left in <p> inside <blockquote>. Eliminated border right for quoted text

--- wp-content/themes/oldpaper-child/page.php
+++ wp-content/themes/oldpaper-child/page.php
@@ -17,7 +17,7 @@
 				<?php endif; ?>
 				
 				<header class="topinfo animated" data-anim="bounceIn">
-					<h1><?php the_title() ?> plus some text</h1>
+					<h1><?php the_title() ?></h1>
 					<?php get_template_part('part', 'textinfo') ?>
 				</header>
 			
